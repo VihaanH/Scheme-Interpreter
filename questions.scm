8528f6321915e735363fd361a69319d90441b3f5
VihaanH <vihaan1406@berkeley.edu>
2024-12-27 17:14:11 +0400

    Add files via upload

---
+++ questions.scm
@@ -0,0 +1,77 @@
+(define (caar x) (car (car x)))
+(define (cadr x) (car (cdr x)))
+(define (cdar x) (cdr (car x)))
+(define (cddr x) (cdr (cdr x)))
+
+;; Problem 15
+;; Returns a list of two-element lists
+(define (enumerate lst)
+  (define (enumerate-helper lst index)
+    (if (null? lst)
+        '()  
+        (cons (list index (car lst))  
+              (enumerate-helper (cdr lst) (+ index 1)))))  
+  (enumerate-helper lst 0))
+
+;; Problem 16
+
+;; Merge two lists S1 and S2 according to ORDERED? and return
+;; the merged lists.
+(define (merge ordered? s1 s2)
+  (cond ((null? s1) s2)  ; If the first list is empty, return the second list.
+        ((null? s2) s1)  ; If the second list is empty, return the first list.
+        ((ordered? (car s1) (car s2))  ; If the first element of s1 is ordered before the first element of s2
+         (cons (car s1) (merge ordered? (cdr s1) s2)))  ; Recur with the rest of s1 and the entire s2.
+        (else
+         (cons (car s2) (merge ordered? s1 (cdr s2))))))
+  ; END PROBLEM 16
+
+;; Optional Problem 2
+
+;; Returns a function that checks if an expression is the special form FORM
+(define (check-special form)
+  (lambda (expr) (equal? form (car expr))))
+
+(define lambda? (check-special 'lambda))
+(define define? (check-special 'define))
+(define quoted? (check-special 'quote))
+(define let?    (check-special 'let))
+
+;; Converts all let special forms in EXPR into equivalent forms using lambda
+(define (let-to-lambda expr)
+  (cond ((atom? expr)
+         ; BEGIN OPTIONAL PROBLEM 2
+         'replace-this-line
+         ; END OPTIONAL PROBLEM 2
+         )
+        ((quoted? expr)
+         ; BEGIN OPTIONAL PROBLEM 2
+         'replace-this-line
+         ; END OPTIONAL PROBLEM 2
+         )
+        ((or (lambda? expr)
+             (define? expr))
+         (let ((form   (car expr))
+               (params (cadr expr))
+               (body   (cddr expr)))
+           ; BEGIN OPTIONAL PROBLEM 2
+           'replace-this-line
+           ; END OPTIONAL PROBLEM 2
+           ))
+        ((let? expr)
+         (let ((values (cadr expr))
+               (body   (cddr expr)))
+           ; BEGIN OPTIONAL PROBLEM 2
+           'replace-this-line
+           ; END OPTIONAL PROBLEM 2
+           ))
+        (else
+         ; BEGIN OPTIONAL PROBLEM 2
+         'replace-this-line
+         ; END OPTIONAL PROBLEM 2
+         )))
+
+; Some utility functions that you may find useful to implement for let-to-lambda
+
+(define (zip pairs)
+  'replace-this-line)
